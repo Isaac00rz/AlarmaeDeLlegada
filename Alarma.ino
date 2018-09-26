51ce1a6932c9fae68636242fd7da30762aa92cc7
Isaac00rz <Isaac_marqz@outlook.com>
2018-09-25 22:20:41 -0500

    Ahorro de energia e interrupcion externa

--- Alarma.ino
+++ Alarma.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <avr/sleep.h>
+#include <avr/power.h>
+int pinRedSw = 3; //Pin usado para leer el Reed Switch
+int pinBoozer = 13; //Pin usado para el sonido de la alarma
 
+
+void setup() {
+  pinMode(pinRedSw, INPUT);
+  pinMode(pinBoozer, OUTPUT);
+  digitalWrite(pinBoozer, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(pinBoozer, LOW);// Cambio del boozer a low
+  
+  attachInterrupt(digitalPinToInterrupt(pinRedSw), despertar,LOW);// Interrupcion para despertar
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();// Activacion del bajo consumo
+  sleep_mode();// Modo dormir
+  //Aqui se duerme el programa, y al despertar empezara desde la linea de abajo
 
+  //Inicio de la alarma 
+  
+}
+
+void despertar(void){
+  detachInterrupt(digitalPinToInterrupt(pinRedSw)); // Detach de la interrupcion para que no se vuelva a despertar 
+  sleep_disable();// Desactivar el modo ahorro de energia
 }
