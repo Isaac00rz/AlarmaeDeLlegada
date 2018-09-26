01b9a7890710a4b8794a0904472526107a999b15
Isaac00rz <Isaac_marqz@outlook.com>
2018-09-26 18:10:33 -0500

    Adaptacion a ReedSwitch

--- Alarma.ino
+++ Alarma.ino
@@ -1,33 +1,34 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
+
 int pinRedSw = 3; //Pin usado para leer el Reed Switch
-int pinBoozer = 13; //Pin usado para el sonido de la alarma
+int pinBuzzer = 13; //Pin usado para el sonido de la alarma
 
 
 void setup() {
-  pinMode(pinRedSw, INPUT);
-  pinMode(pinBoozer, OUTPUT);
-  digitalWrite(pinBoozer, LOW);
+  Serial.begin(9600);
+  pinMode(pinRedSw, INPUT_PULLUP);
+  pinMode(pinBuzzer, OUTPUT);
+  digitalWrite(pinBuzzer, LOW);
 }
 
 void loop() {
-  digitalWrite(pinBoozer, LOW);// Cambio del boozer a low
-  
+  digitalWrite(pinBuzzer, LOW);// Cambio del boozer a low
+  Serial.println("Voy a dormir...");
+  delay(500); // Delay para que cargue el modo sleep power down
   attachInterrupt(digitalPinToInterrupt(pinRedSw), despertar,LOW);// Interrupcion para despertar
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();// Activacion del bajo consumo
   sleep_mode();// Modo dormir
+  
   //Aqui se duerme el programa, y al despertar empezara desde la linea de abajo
-
   //Inicio de la alarma 
-  
+  Serial.println("Desperte!!");
   while(digitalRead(pinRedSw)==LOW){ //Ciclo para que la alarma no se desactive hasta que el estado no pase a high
-    digitalWrite(pinBoozer, HIGH);
+    digitalWrite(pinBuzzer, HIGH);
   }
-  
 }
 
 void despertar(void){
-  detachInterrupt(digitalPinToInterrupt(pinRedSw)); // Detach de la interrupcion para que no se vuelva a despertar 
   sleep_disable();// Desactivar el modo ahorro de energia
 }
